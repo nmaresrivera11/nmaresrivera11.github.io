b7e75ad14f78e9638524b0eeb3745647a8488a5a
Nicole Mares Rivera <107439179+nmaresrivera11@users.noreply.github.com>
2022-11-20 16:22:05 -0800

    Add files via upload
    This JumpStart was AWESOME!

---
+++ cornBread.js
@@ -0,0 +1,15 @@
+// make an alert so I know this page is connected
+alert("this page is connected");
+
+// creating a function that takes in a <td>
+//  and turns that <td>'s inner HTML into an emoji
+
+const emojiMaker = (id) => {
+  if (id === "glass") {
+    document.getElementById(id).innerHTML = "🍸";
+  } else if (id === "ceramic") {
+    document.getElementById(id).innerHTML = "🪴";
+  } else {
+    document.getElementById(id).innerHTML = "🍳";
+  }
+};
