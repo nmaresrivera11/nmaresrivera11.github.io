71be31279e18b4c30d8f9c71f3a80b5f91dd03e8
Nicole Mares Rivera <107439179+nmaresrivera11@users.noreply.github.com>
2022-11-20 16:29:59 -0800

    Add files via upload
    removed alert from cornBread.js

--- cornBread.js
+++ cornBread.js
@@ -1,5 +1,4 @@
 // make an alert so I know this page is connected
-alert("this page is connected");
 
 // creating a function that takes in a <td>
 //  and turns that <td>'s inner HTML into an emoji
